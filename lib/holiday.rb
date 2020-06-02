23ac7e671065e286cc81d4d4224e72fcdd1acacb
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2020-06-02 01:00:04 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -79,6 +79,8 @@ end
   # etc.
 
 def all_holidays_with_bbq(holiday_hash)
+  holiday_hash[seasons]
+  binding.pry
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 
